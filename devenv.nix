865a920c24da8675eee50525fd9c237f6953ca68
delirial <elarcadeldelirio@gmail.com>
2024-04-11 12:15:55 +0200

    :bricks: local dev and dx improvements

---
+++ devenv.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
+  # https://devenv.sh/packages/
+  packages = [ 
+    pkgs.poetry
+    pkgs.git
+    pkgs.python310      
+  ];
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "p";
+
+  enterShell = ''
+    poetry --version
+    python --version
+    git --version
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    wait_for_port 8888
+    curl -s localhost:8888/lab
+  '';
+              
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  processes.jupyter.exec = "poetry run jupyter lab";
+  devcontainer.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
